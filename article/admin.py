fa47a7103920126be09cfaf46eb0a61a5615d5f6
pycoder404 <pycoder404@gmail.com>
2022-07-26 06:44:19 -0700

    register category and tag model in admin site

--- article/admin.py
+++ article/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from article.models import Article
+from article.models import Article,Category,Tag
 admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Tag)
